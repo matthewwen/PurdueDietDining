35582a51550170d7d6136a48fc172a76ddd87946
Matthew Wen <mattwen2018@gmail.com>
2018-09-15 16:24:42 -0400

    Added Setting View inside the project

--- app/src/main/java/com/purduediet/helloworld/purduedietdining/userInterface/MainActivity.java
+++ app/src/main/java/com/purduediet/helloworld/purduedietdining/userInterface/MainActivity.java
@@ -1,5 +1,6 @@
 package com.purduediet.helloworld.purduedietdining.userInterface;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,11 +11,26 @@ import android.widget.TextView;
 
 import com.purduediet.helloworld.purduedietdining.R;
 import com.purduediet.helloworld.purduedietdining.adapter.MainAdapter;
+import com.purduediet.helloworld.purduedietdining.objects.ItemFood;
+import com.purduediet.helloworld.purduedietdining.preference.SettingsActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+        switch (itemID){
+            case R.id.action_calendar: {
+                break;
+            }
+            case R.id.action_settings: {
+                Intent intent = new Intent(this,SettingsActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
         return true;
     }
 
@@ -23,7 +39,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MainAdapter mAdapter = new MainAdapter();
+        ArrayList<ItemFood> allItemFood = new ArrayList<>();
+        allItemFood.add(new ItemFood("Hamburger"));
+        allItemFood.add(new ItemFood("Pizza"));
+
+        MainAdapter mAdapter = new MainAdapter(allItemFood);
         RecyclerView recyclerView = findViewById(R.id.main_view_rv);
         LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
