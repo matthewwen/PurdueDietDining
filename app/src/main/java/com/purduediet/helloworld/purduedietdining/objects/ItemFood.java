6efc0c70ca323b14555ecfd625e730723e061aae
aarya2000 <abarve@purdue.edu>
2018-09-15 14:42:41 -0400

    Test-commit

--- app/src/main/java/com/purduediet/helloworld/purduedietdining/objects/ItemFood.java
+++ app/src/main/java/com/purduediet/helloworld/purduedietdining/objects/ItemFood.java
@@ -1,4 +1,14 @@
 package com.purduediet.helloworld.purduedietdining.objects;
 
 public class ItemFood {
+
+    private String name;
+
+    public ItemFood(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
