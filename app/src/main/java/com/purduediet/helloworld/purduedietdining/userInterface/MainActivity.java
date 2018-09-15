1d670528d9532ccd1d08888eb8f0a23e40ee9c1f
Matthew Wen <mattwen2018@gmail.com>
2018-09-15 17:10:15 -0400

    Added More Food as an Example

--- app/src/main/java/com/purduediet/helloworld/purduedietdining/userInterface/MainActivity.java
+++ app/src/main/java/com/purduediet/helloworld/purduedietdining/userInterface/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(mAdapter);
 
+        String name = ""; 
     }
 
     @Override
