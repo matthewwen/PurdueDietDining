68fe4da562fa998a49992b262d2a7fcb2eded29f
shreyas <kulkar48@purdue.edu>
2018-09-15 20:05:21 -0400

    Added create table for their db helper

--- app/src/main/java/com/purduediet/helloworld/purduedietdining/database/DataHelper.java
+++ app/src/main/java/com/purduediet/helloworld/purduedietdining/database/DataHelper.java
@@ -3,6 +3,7 @@ package com.purduediet.helloworld.purduedietdining.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.purduediet.helloworld.purduedietdining.database.DataContract.Food;
 
 public class DataHelper extends SQLiteOpenHelper {
 
@@ -12,6 +13,17 @@ public class DataHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        final String USER_CREATE_TABLE = "CREATE TABLE " + Food.TABLE_NAME + " (" +
+                Food._ID + " INTEGER PRIMARY KEY, " +
+                Food.COLUMN_TIME_ADDED + " LONG NOT NULL, " +
+                Food.COLUMN_FOOD_NAME + " TEXT NOT NULL, " +
+                Food.COLUMN_DINING_COURT + "TEXT NOT NULL," +
+                Food.COLUMN_LOCATION + "TEXT NOT NULL," +
+                Food.COLUMN_MEAL_TYPE + "TEXT NOT NULL,"+
+                Food.COLUMN_CALORIES + "INTEGERS NOT NULL,"+
+                Food.COLUMN_PROTEIN + "INTEGERS NOT NULL);";
+        db.execSQL(USER_CREATE_TABLE);
 
     }
 
