0054cc2def782202d19de1161ae1c0526a0026bb
CJ1067 <38930150+CJ1067@users.noreply.github.com>
2018-09-16 04:48:30 -0400

    Backup!! DO NOT DELETE!!

--- app/src/main/java/com/purduediet/helloworld/purduedietdining/objects/ItemFood.java
+++ app/src/main/java/com/purduediet/helloworld/purduedietdining/objects/ItemFood.java
@@ -2,27 +2,35 @@ package com.purduediet.helloworld.purduedietdining.objects;
 
 public class ItemFood {
 
+    private String diningCourt;
+    private String timeOfDay;
+    private String location;
     private String name;
-    private String description;
     private int calories;
+    private int protein;
+    private boolean isVegetarian;
+    private boolean isGlutenFree;
 
     public ItemFood(String name){
         this.name = name;
     }
 
-    public ItemFood(String name, String description, int calories){
-        this.name = name; this.description = description; this.calories = calories;
+    public ItemFood(String diningCourt, String timeOfDay, String location, String name, int calories, int protein, boolean isVegetarian, boolean isGlutenFree){
+        this.diningCourt = diningCourt; this.timeOfDay = timeOfDay; this.location = location;
+        this.name = name; this.calories = calories; this.protein = protein; this.isVegetarian = isVegetarian; this.isGlutenFree = isGlutenFree;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getDescription(){
-        return description;
+    public int getProtein(){
+        return protein;
     }
 
     public int getCalories(){
         return calories;
     }
+
+
 }
