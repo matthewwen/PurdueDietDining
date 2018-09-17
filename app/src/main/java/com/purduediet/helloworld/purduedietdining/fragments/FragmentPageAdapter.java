85a21426b4f1c0bb12346ea8fa4b18e4153650f4
Matthew Wen <mattwen2018@gmail.com>
2018-09-17 01:40:48 -0400

    Created Fragments for the main view (it will spit between breakfast, lunch, and dinner)

---
+++ app/src/main/java/com/purduediet/helloworld/purduedietdining/fragments/FragmentPageAdapter.java
@@ -0,0 +1,22 @@
+package com.purduediet.helloworld.purduedietdining.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FragmentPageAdapter extends FragmentPagerAdapter {
+
+    public FragmentPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
